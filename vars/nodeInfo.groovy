54fd09149189c60217f1e77ca0a1d9a283c7bb67
Csaba Soti <csaba.soti@dotmatics.com>
2019-05-30 14:57:37 +0100

    2019-05-30 14:57:37

--- vars/nodeInfo.groovy
+++ vars/nodeInfo.groovy
@@ -2,7 +2,7 @@
 def call() {
     sh "!!!!hostname || true"
     sh "whoami || true"
-    sh "id || true"
+    sh_master "id || true"
     sh "pwd || true"
     sh "env || true"
     sh "curl ifconfig.co || true"
